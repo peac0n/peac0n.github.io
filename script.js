1ba2242ee08947bbd064340fa08fd1be98766d48
Beacon <qaiseralhashmi@gmail.com>
2025-02-22 16:12:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
-const design_card_butttons = document.querySelectorAll('.design-card');
-const introduction_text = document.querySelectorAll('.introduction-text');
-
-const single_profile_card = document.querySelectorAll('.single-profile-card');
-const testimonial_card = document.querySelectorAll('.testimonial-card');
-
-design_card_butttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        introduction_text.forEach((introduction, introductionIndex) => {
-            if (index === introductionIndex) {
-                introduction.style.display = 'block';
-            } else {
-                introduction.style.display = 'none';
-            }
-        });
-        design_card_butttons.forEach((btn, btnIndex) => {
-            if (index === btnIndex) {
-                btn.classList.add('active');
-            } else {
-                btn.classList.remove('active');
-                
-            }
-        });
-    });
-});
-
-single_profile_card.forEach((btn, index) => {
-    btn.addEventListener('click', ()=> {
-        testimonial_card.forEach((testimonialCard, testimonialCardIndex) => {
-            if (index === testimonialCardIndex) {
-                testimonialCard.style.display = 'block';
-            } else {
-                testimonialCard.style.display = 'none';
-            }
-        });
-        single_profile_card.forEach((cardBtn, cardIndex) => {
-            if (index === cardIndex) {
-                cardBtn.classList.add('profile-card-active');
-            } else {
-                cardBtn.classList.remove('profile-card-active');
-            }
-        });
-    });
-});
+window.addEventListener("DOMContentLoaded",function(){
+    setTimeout(function(){
+      document.getElementById("loaderWrapper").style.display="none";
+      document.getElementById("mainContent").style.display="block";
+    },3000);
+  });
+  
+  let tela=document.createElement('canvas');
+  tela.width=$(window).width();
+  tela.height=$(window).height();
+  $("body").append(tela);
+  let canvas=tela.getContext('2d');
+  class Particle{
+    constructor(c,p){
+      let r=Math.random();
+      this.p=0;this.c=c;
+      this.x=$(window).width()/2+(Math.random()*200-Math.random()*200);
+      this.y=$(window).height()/2+(Math.random()*200-Math.random()*200);
+      this.w=$(window).width();
+      this.h=$(window).height();
+      this.radius=r>.2?Math.random()*1:Math.random()*3;
+      this.color=r>.2?"#d8002c":"#F9314C";
+      this.radius=r>.8?Math.random()*2:this.radius;
+      this.color=r>.8?"#7DFFF2":this.color;
+      this.vx1=Math.random()*300;
+      this.vx2=Math.random()*400;
+      this.vy1=Math.random()*100;
+      this.vy2=Math.random()*120;
+    }
+    render(){
+      this.c.beginPath();
+      this.c.arc(this.x,this.y,this.radius,0,2*Math.PI);
+      this.c.lineWidth=2;
+      this.c.fillStyle=this.color;
+      this.c.fill();
+      this.c.closePath();
+    }
+    move(){
+      this.x+=(Math.sin(this.p/this.vx1)*Math.cos(this.p/this.vx2));
+      this.y+=(Math.cos(this.p/this.vy2));
+      if(this.x<0||this.x>this.w-this.radius)return false;
+      if(this.y<0||this.y>this.h-this.radius)return false;
+      this.render();this.p++;return true;
+    }
+  }
+  let particles=[],m=popolate(1000);
+  function popolate(n){
+    for(let i=0;i<n;i++){
+      setTimeout(function(){
+        particles.push(new Particle(canvas,i));
+      },i*20);
+    }
+    return particles.length;
+  }
+  function clear(){
+    canvas.globalAlpha=0.05;
+    canvas.fillStyle='#2e050d';
+    canvas.fillRect(0,0,tela.width,tela.height);
+    canvas.globalAlpha=1;
+  }
+  function update(){
+    particles=particles.filter(function(x){return x.move()});
+    if(particles.length<m)popolate(1);
+    clear();
+    requestAnimationFrame(update);
+  }
+  update();
+  
